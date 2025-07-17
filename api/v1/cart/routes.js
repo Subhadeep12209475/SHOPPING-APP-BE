9378c4c8c1e3334896f4718558795ecd3172537e
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-07-18 01:49:45 +0530

    routesChanges

--- api/v1/cart/routes.js
+++ api/v1/cart/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { addToCart, getCartItems, removeFromCart } = require('./controllers');
 
+// No prefix here. Keep routes clean.
 router.post('/add', addToCart);
 router.get('/item', getCartItems);
 router.delete('/remove/:productId', removeFromCart);
